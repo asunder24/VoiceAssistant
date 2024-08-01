88c983cbac460a81b197b479abc501a7ca748af7
asunder24 <asunder24@github.com>
2024-07-31 22:43:53 -0700

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import os
+
+def main():
+    #Initialize gpt
+    #Initialize system
+    #Take in user input through voice
+    #Process to text
+    #Check query to determine if system process or search
+    #Feed text to model
+    #Receive response, output as speech and text
+    #
+
+
+if __name__ == "__main__":
+    main()
