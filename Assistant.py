95f72d4f28c4247aabfad2c2f1ff8ab635f6c31d
asunder24 <ajiteshsunder24@gmail.com>
2024-08-01 19:26:04 -0700

    Adding basic functionality (e.g. toggling state) and fooundation for tts

--- Assistant.py
+++ Assistant.py
@@ -6,6 +6,7 @@ class Assistant:
     def __init__(self, key):
         #self.client = OpenAI(api_key=key)
         #openai.api_key = key
+        self.voice = False
         self.history = [{"role": "system", "content": "You are a personal assistant. Use only English. Provide helpful responses. Be as concise as possible."}]
 
 
@@ -14,16 +15,36 @@ class Assistant:
     def listen(self):
         recognizer = sr.Recognizer()
         with sr.Microphone() as user_input:
-            print("Listening for a query...")
+            self.speak("Listening for a query...")
             audio = recognizer.listen(user_input)
         try:
             #  return recognizer.recognize_sphinx(audio) -- nonfunctional for now, issues with sphinx installation
             return recognizer.recognize_google(audio) # Add a specific API key functionality later, the default could be inconsistent
         except sr.UnknownValueError:
-            print("Sorry, I couldn't quite understand that. Please try again.")
+            self.speak("Sorry, I couldn't quite understand that. Please try again.")
             return ""
         except sr.RequestError:
-            print("Error! Please try again later.")
+            self.speak("Error! Please try again later.")
             return ""
 
+    def process_query(self, query){
+        #feed query into gpt model
+        #provide list of tool functions so model can call if necessary
+        #use error checking the ensure no hallucination
+        return None
+    }
+    
+    def toggle_voice(self):
+        self.voice = not self.voice
+        if self.voice == True:
+            self.speak("Activating text to speech...")
+            return None
+        self.speak("Deactivating text to speech...")
+
+    def speak(self, response):
+        if self.voice == False:
+            print(response)
+            return None
+        #else text to speech with model response
+    
 
